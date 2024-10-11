182abd0cdfcd4a4bf91b43e8068bc9ed39721d50
Ankit Gupta <122438168+xq-ankit@users.noreply.github.com>
2024-10-11 13:47:48 +0530

    Time: 111 ms (57.31%), Space: 84.3 MB (48.33%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -41,3 +41,41 @@ while(!st.empty()){
 
     }
 };
+
+
+
+
+
+
+
+
+
+
+// Approch 2 :
+// in one iteration in pse the element we are pop for them the new element is the nse.
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& a ) {
+        int n=a.size();
+        stack<int>st;
+        vector<int>pse;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && a[st.top()]>=a[i]){
+                int element=st.top();st.pop();
+                int nse=i;
+                int pse=st.empty()?-1:st.top();
+                int area=a[element]*(nse-pse-1);
+                ans=max(ans,area); }
+                st.push(i);
+        }
+        while(!st.empty()){
+                int element=st.top();st.pop();
+                int nse=n;
+                int pse=st.empty()?-1:st.top();
+                int area=a[element]*(nse-pse-1);
+                ans=max(ans,area); 
+        }
+        return ans;
+    }
+};
