8b803bd340810ee28388df198a3168ed86bab643
Ankit Gupta <122438168+xq-ankit@users.noreply.github.com>
2024-10-15 21:12:34 +0530

    Time: 29 ms (37.53%), Space: 30.2 MB (72.58%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int start=0;
+        int end=arr.size()-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+
+        }
+        return -1;
+    }
+};
