ceae7b7ca734b1d4edbd9eeeba78d8bd34e20488
Ankit Gupta <122438168+xq-ankit@users.noreply.github.com>
2024-10-23 12:25:14 +0530

    Time: 91 ms (46.67%), Space: 13.2 MB (50.33%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -15,4 +15,21 @@ public:
         int ans=*max_element(dp.begin(),dp.end());
         return ans;
     }
+
+
+    int LISusingBinarySearch(vector<int>& nums,vector<int>& dp){
+        dp[0]=nums[0];
+        int len=1;
+        for(int i=1;i<dp.size();i++){
+            if(nums[i]>dp[len-1]){
+                dp[len]=nums[i];
+                len++;
+            }
+            else{
+            int idx=*lower_bound(dp.begin(),dp.begin() + len,nums[i]);
+            dp[idx]=nums[i];
+            }
+        }
+        return len;
+    }
 };
